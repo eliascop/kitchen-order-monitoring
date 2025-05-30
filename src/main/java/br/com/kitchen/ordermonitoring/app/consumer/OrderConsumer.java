299cdaddc5eca2d0b254383e64dbb65bcb5774c6
elias <eliascop@yahoo.com.br>
2025-05-30 18:05:17 -0300

    [kitchen-order-monitoring-04] - Melhorias de codigo e remocao de config dos properties

--- src/main/java/br/com/kitchen/ordermonitoring/app/consumer/OrderConsumer.java
+++ src/main/java/br/com/kitchen/ordermonitoring/app/consumer/OrderConsumer.java
@@ -2,41 +2,24 @@ package br.com.kitchen.ordermonitoring.app.consumer;
 
 import br.com.kitchen.ordermonitoring.app.dto.OrderDTO;
 import br.com.kitchen.ordermonitoring.app.websocket.OrderWebSocket;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderConsumer {
+public class OrderConsumer extends KafkaGenericConsumer<OrderDTO> {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    @KafkaListener(topics = "new-order", groupId = "order-monitoring-consumer-new-order")
-    public void listenNewOrder(String strOrder) {
-        try {
-            OrderDTO orderDTO = objectMapper.readValue(strOrder, OrderDTO.class);
-            if(orderDTO.getId() != null && !orderDTO.getStatus().isEmpty()) {
-                OrderWebSocket.notifyOrderUpdate(orderDTO);
-            }else{
-                System.err.println("Invalid message: "+strOrder);
-            }
-        } catch (JsonProcessingException e) {
-            System.err.println("Poorly formatted message: " + strOrder);
-        }
+    public OrderConsumer() {
+        super(OrderDTO.class, OrderWebSocket::notifyOrderUpdate);
     }
 
+    @KafkaListener(topics = "new-order", groupId = "order-monitoring-consumer-new-order")
+    public void listenNewOrder(String message) {consume(message);}
+
     @KafkaListener(topics = "order-status-updates", groupId = "order-monitoring-consumer-order-update")
-    public void listenOrderUpdates(String strNewOrderStatus) {
-        try{
-            OrderDTO orderDTO = objectMapper.readValue(strNewOrderStatus, OrderDTO.class);
-            if(orderDTO.getId() != null && !orderDTO.getStatus().isEmpty()) {
-                OrderWebSocket.notifyOrderUpdate(orderDTO);
-            }else{
-                System.err.println("Invalid message: "+strNewOrderStatus);
-            }
-        } catch (JsonProcessingException e) {
-            System.err.println("Poorly formatted message: " + strNewOrderStatus);
-        }
+    public void listenOrderUpdates(String message) {consume(message);}
+
+    @Override
+    protected boolean isValid(OrderDTO orderDTO) {
+        return orderDTO.getId() != null && orderDTO.getStatus() != null && !orderDTO.getStatus().isEmpty();
     }
 }
